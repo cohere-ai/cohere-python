09d1cfe571c244694f2c29ef921e9823814b23e5
Carol Chen <hello@carolchen.me>
2021-02-10 10:16:59 -0500

    update sanity

--- sanity-test.py
+++ sanity-test.py
@@ -24,11 +24,11 @@ similarities = co.similarity(
             targets=["co:here", "cohere"])
 print('Similarity value of `co:here`: {}'.format(similarities[0]))
 
-best_options = co.choose_best(
+options = co.choose_best(
             model="baseline-likelihood",
             query="hello {}",
             options=["world", "cohere"])
-print('Best option is `{}`, with likelihood value of {}'.format(best_options['rankedOptions'][0]['option'], best_options['rankedOptions'][0]['likelihood']))
+print('first option is `world`, with likelihood value of {}'.format(best_options['likelihoods'][0]))
 print('Selected mode was {}'.format(best_options['mode']))
 
 try:
