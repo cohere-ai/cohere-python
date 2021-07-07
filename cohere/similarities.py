bfd05ba1152a23c9ae8d25131582e8be7b04e51b
Jeff <jeffrey-hui@users.noreply.github.com>
2021-07-07 17:21:17 -0400

    Add Iterators to Custom Objects (#19)

--- cohere/similarities.py
+++ cohere/similarities.py
@@ -3,6 +3,13 @@ from typing import List
 class Similarities:
     def __init__(self, similarities: List[float]) -> None:
         self.similarities = similarities
+        self.iterator = iter(similarities)
 
     def __str__(self) -> str:
         return str(self.similarities)
+
+    def __iter__(self) -> iter:
+        return self.iterator
+
+    def __next__(self) -> next:
+        return next(self.iterator)
--- sanity-test.py
+++ sanity-test.py
@@ -6,23 +6,37 @@ import cohere
 
 co = cohere.CohereClient(API_KEY)
 
+
+### GENERATE
 prediction = co.generate(
             model="baseline-shrimp",
             prompt="co:here",
             max_tokens=10)
 print('prediction: {}'.format(prediction.text))
 
+
+### EMBED
 embeddings = co.embed(
             model="baseline-shrimp",
             texts=["co:here", "cohere"])
 print('Embedding of `co:here`: {}'.format(embeddings.embeddings[0]))
 
+for em in embeddings:
+      print(em)
+
+
+### SIMILARITY
 similarities = co.similarity(
             model="baseline-shrimp",
            	anchor="cohere ai",
             targets=["co:here", "cohere"])
 print('Similarity value of `co:here`: {}'.format(similarities.similarities[0]))
 
+for sim in similarities:
+      print(sim)
+
+
+### CHOOSE BEST
 options = co.choose_best(
             model="baseline-shrimp",
             query="hello {}",
@@ -31,6 +45,11 @@ options = co.choose_best(
 print('first option is `world`, with likelihood value of {}'.format(options.likelihoods[0]))
 print('Selected mode was {}'.format(options.mode))
 
+for op in options:
+      print(op)
+
+
+### LIKELIHOOD
 likelihood = co.likelihood(
             model="baseline-shrimp",
             text="hello, my name is johnny SURPRISE")
@@ -39,6 +58,8 @@ print('token likelihoods are: (first token has no likelihood)')
 for token in likelihood.token_likelihoods:
       print(token['token'], token.get('likelihood', ''))
 
+
+### ERRORS
 try:
 	predictions = co.generate(
             model="fake-model",
