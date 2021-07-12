de80b019e999771337dfc4a2545d27aed82db40f
Robert Kozin <robert@robertkozin.com>
2021-07-12 11:23:55 -0400

    Fix missing renames

--- cohere/best_choices.py
+++ cohere/best_choices.py
@@ -4,10 +4,10 @@ class BestChoices:
     def __init__(self, scores: List[float], mode: str) -> None:
         self.scores = scores
         self.mode = mode
-        self.iterator = iter(likelihoods)
+        self.iterator = iter(scores)
     
     def __str__(self) -> str:
-        return str(self.likelihoods)
+        return str(self.scores)
 
     def __iter__(self) -> iter:
         return self.iterator
