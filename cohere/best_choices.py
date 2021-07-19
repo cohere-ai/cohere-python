c6b25b7f2e600682cf3e3bfae4d2327d55eb9484
Robert Kozin <robert@robertkozin.com>
2021-07-19 14:58:50 -0400

    Rename ChooseBest output `likelihoods` to `scores` (#21)
    Co-authored-by: Jim Wu <jim.wu6@outlook.com>

--- cohere/best_choices.py
+++ cohere/best_choices.py
@@ -1,13 +1,13 @@
 from typing import List
 
 class BestChoices:
-    def __init__(self, likelihoods: List[float], mode: str) -> None:
-        self.likelihoods = likelihoods
+    def __init__(self, scores: List[float], mode: str) -> None:
+        self.scores = scores
         self.mode = mode
-        self.iterator = iter(likelihoods)
+        self.iterator = iter(scores)
     
     def __str__(self) -> str:
-        return str(self.likelihoods)
+        return str(self.scores)
 
     def __iter__(self) -> iter:
         return self.iterator
