90b425731d02e41e79007d1e858d836918b6cbd9
Jim Wu <jim.wu6@outlook.com>
2021-11-15 18:49:38 -0500

    Add Multigen (#34)
    * change quotes
    
    * put in multigen token llhs
    
    * change model names
    
    * change naming of Client side version variable
    
    * update changelog and readme
    
    * update CHANGELOG
    
    * update CHANGELOG
    
    * update CHANGELOG
    
    * default version to latest
    
    * change version
    
    * change model name
    
    * fix errors

--- setup.py
+++ setup.py
@@ -1,25 +1,25 @@
 import setuptools
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="cohere",
-    version="0.0.13",
-    author="kipply",
-    author_email="carol@cohere.ai",
-    description="A Python library for the Cohere API",
+    name='cohere',
+    version='1.0.0',
+    author='kipply',
+    author_email='carol@cohere.ai',
+    description='A Python library for the Cohere API',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/cohere-ai/cohere-python",
+    long_description_content_type='text/markdown',
+    url='https://github.com/cohere-ai/cohere-python',
     packages=setuptools.find_packages(),
     install_requires=[
         'requests'
     ],
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
     python_requires='>=3.6',
 )
