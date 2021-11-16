0a2f988593ad0730685e3a064ab675415ca7ebda
kipply <carol@cohere.ai>
2021-11-16 15:26:32 -0500

    reformat multi gen to be same as rest, add tests

--- cohere/generation.py
+++ cohere/generation.py
@@ -1,8 +1,14 @@
+from cohere.likelihoods import TokenLikelihood
 from cohere.response import CohereResponse
 from typing import List, Dict
 
-class Generation(CohereResponse):
-    def __init__(self, texts: List[str], token_likelihoods: List[List[Dict]], return_likelihoods: str) -> None:
-        self.texts = texts
+
+class Generation:
+    def __init__(self, text: str, token_likelihoods: TokenLikelihood) -> None:
+        self.text = text
         self.token_likelihoods = token_likelihoods
+
+class Generations(CohereResponse):
+    def __init__(self, generations: List[Generation], return_likelihoods: str) -> None:
+        self.generations = generations
         self.return_likelihoods = return_likelihoods
--- cohere/likelihoods.py
+++ cohere/likelihoods.py
@@ -1,7 +1,13 @@
 from cohere.response import CohereResponse
 from typing import List, Dict
 
+
+class TokenLikelihood: 
+    def __init__(self, token: str, likelihood: float) -> None:
+        self.token = token
+        self.likelihood = likelihood 
+
 class Likelihoods(CohereResponse):
-    def __init__(self, likelihood: float, token_likelihoods: List[Dict]) -> None:
+    def __init__(self, likelihood: float, token_likelihoods: List[TokenLikelihood]) -> None:
         self.likelihood = likelihood
         self.token_likelihoods = token_likelihoods
