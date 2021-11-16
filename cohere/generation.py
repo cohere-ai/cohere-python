f49b53266dc5349d544a41d1659fb2196579a086
kipply <carol@cohere.ai>
2021-11-16 16:29:03 -0500

    allow pretty print

--- cohere/generation.py
+++ cohere/generation.py
@@ -1,14 +1,14 @@
 from cohere.likelihoods import TokenLikelihood
-from cohere.response import CohereResponse
-from typing import List, Dict
+from cohere.response import CohereObject
+from typing import List
 
 
-class Generation:
+class Generation(CohereObject):
     def __init__(self, text: str, token_likelihoods: TokenLikelihood) -> None:
         self.text = text
         self.token_likelihoods = token_likelihoods
 
-class Generations(CohereResponse):
+class Generations(CohereObject):
     def __init__(self, generations: List[Generation], return_likelihoods: str) -> None:
         self.generations = generations
         self.return_likelihoods = return_likelihoods
--- cohere/likelihoods.py
+++ cohere/likelihoods.py
@@ -1,13 +1,13 @@
-from cohere.response import CohereResponse
-from typing import List, Dict
+from cohere.response import CohereObject
+from typing import List
 
 
-class TokenLikelihood: 
+class TokenLikelihood(CohereObject): 
     def __init__(self, token: str, likelihood: float) -> None:
         self.token = token
         self.likelihood = likelihood 
 
-class Likelihoods(CohereResponse):
+class Likelihoods(CohereObject):
     def __init__(self, likelihood: float, token_likelihoods: List[TokenLikelihood]) -> None:
         self.likelihood = likelihood
         self.token_likelihoods = token_likelihoods
