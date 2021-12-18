7a101600c3cb6b0719ccb86aa7d7e4c27fc6daf9
Carol Chen <hello@carolchen.me>
2021-12-17 23:59:39 -0500

    Remove similarity (#39)
    * bump version to 1.0.1
    
    * remove similarity endpoint
    
    * remove similarity endpoint
    
    * remove from more places
    
    * merge from main
    
    Co-authored-by: Jim Wu <jim.wu6@outlook.com>

--- cohere/__init__.py
+++ cohere/__init__.py
@@ -4,7 +4,6 @@ from .error import CohereError
 COHERE_API_URL = 'https://api.cohere.ai'
 COHERE_VERSION = '2021-11-08'
 GENERATE_URL = 'generate'
-SIMILARITY_URL = 'similarity'
 EMBED_URL = 'embed'
 CHOOSE_BEST_URL = 'choose-best'
 LIKELIHOOD_URL = 'likelihood'
