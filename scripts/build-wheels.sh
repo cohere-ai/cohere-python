79e53dceaf1f48f247263d304a31037ed5cedd35
Alex Guo <alexguo247@gmail.com>
2022-02-09 11:28:38 -0500

    python version fix for pybindgen

--- scripts/build-wheels.sh
+++ scripts/build-wheels.sh
@@ -10,7 +10,7 @@ export PATH=$PATH:/usr/local/go/bin
 for PYBIN in /opt/python/{cp36-cp36m,cp37-cp37m,cp38-cp38,cp39-cp39, cp310-cp310}/bin; do
     export PYTHON_SYS_EXECUTABLE="$PYBIN/python"
 
-    python3 -m pip install pybindgen
+   "${PYBIN}/python" -m pip install pybindgen
 
     go get golang.org/x/tools/cmd/goimports
     go get github.com/go-python/gopy
