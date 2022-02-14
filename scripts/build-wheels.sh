dfdb97dc4011260eaf2818529570eed20de3cff7
ivan <ivan@ivanzhang.ca>
2022-02-14 10:15:31 -0500

    test this GOPATH

--- scripts/build-wheels.sh
+++ scripts/build-wheels.sh
@@ -16,8 +16,9 @@ for PYBIN in /opt/python/{cp36-cp36m,cp37-cp37m,cp38-cp38,cp39-cp39,cp310-cp310}
     go get golang.org/x/tools/cmd/goimports
     go get github.com/go-python/gopy
     go get github.com/cohere-ai/tokenizer
-    cd $GOPATH/github.com/cohere-ai/tokenizer
-    ~/go/bin/gopy build -output=tokenizer -vm=python3 $GOPATH/github.com/cohere-ai/tokenizer
+    ls $GOPATH
+    cd $GOPATH/src/github.com/cohere-ai/tokenizer
+    ~/go/bin/gopy build -output=tokenizer -vm=python3 $GOPATH/src/github.com/cohere-ai/tokenizer
     "${PYBIN}/python" setup.py bdist_wheel
 
     rm -rf build/*
