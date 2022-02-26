49e919b7950398b78195ebe8fe21e386591d73fe
Alex Guo <alexguo247@gmail.com>
2022-02-26 17:48:26 -0500

    try this

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setuptools.setup(
     python_requires='>=3.6',
     ext_modules=[
         setuptools.Extension(
-            name='tokenizer',
+            name='./cohere/tokenizer/',
             sources=[]
         )
     ]
