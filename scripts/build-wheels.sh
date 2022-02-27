33e60c7dbb0f03e95b1a39ff708a77291d0b3481
Alex Guo <alexguo247@gmail.com>
2022-02-26 19:08:32 -0500

    testing

--- scripts/build-wheels.sh
+++ scripts/build-wheels.sh
@@ -5,20 +5,20 @@ curl -O https://storage.googleapis.com/golang/go1.17.6.linux-amd64.tar.gz
 file go1.17.6.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.17.6.linux-amd64.tar.gz
 gcc -v
-# for PYVER in 3.6.15; do
-# # for PYVER in 3.6.15 3.7.9 3.8.10 3.9.10 3.10.2; do
-#     curl -O https://www.python.org/ftp/python/$PYVER/Python-$PYVER.tgz && \
-#                 tar xzf Python-$PYVER.tgz && \
-#                 cd Python-$PYVER && \
-#                 ./configure --enable-optimizations --enable-shared && \
-#                 make altinstall && \
-#                 cd /
-# done
+for PYVER in 3.6.15; do
+# for PYVER in 3.6.15 3.7.9 3.8.10 3.9.10 3.10.2; do
+    curl -O https://www.python.org/ftp/python/$PYVER/Python-$PYVER.tgz && \
+                tar xzf Python-$PYVER.tgz && \
+                cd Python-$PYVER && \
+                ./configure --enable-optimizations --enable-shared && \
+                make altinstall && \
+                cd /
+done
 
-# export PATH=$PATH:/usr/local/go/bin
-# export GOPATH=$HOME/go
-# export GOBIN=$GOPATH/bin
-# export PATH=$PATH:$GOBIN
+export PATH=$PATH:/usr/local/go/bin
+export GOPATH=$HOME/go
+export GOBIN=$GOPATH/bin
+export PATH=$PATH:$GOBIN
 
 mkdir -p $GOPATH/src/github.com/cohere-ai
 for PYBIN in /opt/python/cp36-cp36m/bin; do
@@ -36,6 +36,7 @@ for PYBIN in /opt/python/cp36-cp36m/bin; do
     ls
     cp -a $GOPATH/src/github.com/cohere-ai/tokenizer/tokenizer $GITHUB_WORKSPACE/cohere
     cd $GITHUB_WORKSPACE
+    ls cohere/tokenizer
     "${PYBIN}/python" setup.py bdist_wheel
     rm -rf build/*
     rm -rf $GOPATH/src/github.com/cohere-ai/tokenizer
@@ -44,7 +45,6 @@ done
 
 export LD_LIBRARY_PATH=$GITHUB_WORKSPACE/cohere/tokenizer
 for whl in dist/*.whl; do
-    auditwheel show "$whl"
     auditwheel repair "$whl" -w dist/
 done
 
