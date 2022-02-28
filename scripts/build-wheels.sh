b443e7af0ee91ee6f202073ff699420c30d1ac01
Alex Guo <alexguo247@gmail.com>
2022-02-28 12:26:31 -0500

    try this

--- scripts/build-wheels.sh
+++ scripts/build-wheels.sh
@@ -5,7 +5,8 @@ curl -O https://storage.googleapis.com/golang/go1.17.6.linux-amd64.tar.gz
 file go1.17.6.linux-amd64.tar.gz
 tar -C /usr/local -xzf go1.17.6.linux-amd64.tar.gz
 
-for PYVER in 3.6.15 3.7.9 3.8.10 3.9.10 3.10.2; do
+for PYVER in 3.6.15; do
+# for PYVER in 3.6.15 3.7.9 3.8.10 3.9.10 3.10.2; do
     curl -O https://www.python.org/ftp/python/$PYVER/Python-$PYVER.tgz && \
                 tar xzf Python-$PYVER.tgz && \
                 cd Python-$PYVER && \
@@ -20,7 +21,8 @@ export GOBIN=$GOPATH/bin
 export PATH=$PATH:$GOBIN
 
 mkdir -p $GOPATH/src/github.com/cohere-ai
-for PYBIN in /opt/python/{cp36-cp36m,cp37-cp37m,cp38-cp38,cp39-cp39,cp310-cp310}/bin; do
+for PYBIN in /opt/python/cp36-cp36m/bin; do
+# for PYBIN in /opt/python/{cp36-cp36m,cp37-cp37m,cp38-cp38,cp39-cp39,cp310-cp310}/bin; do
     export PYTHON_SYS_EXECUTABLE="$PYBIN/python"
     cd $GOPATH/src/github.com/cohere-ai && git clone https://github.com/cohere-ai/tokenizer.git && cd $GITHUB_WORKSPACE
 
