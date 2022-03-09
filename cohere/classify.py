b415be910ac38bbe047cd6ec5ba9df8eb9b1f3e0
choicallum <77260697+choicallum@users.noreply.github.com>
2022-03-09 16:23:37 -0500

    feat: Co.classify Python SDK (#52)
    * initial co.classify draft
    
    * feat: co.classify()
    
    * Update README.md
    
    * fix: co.classify() python SDK addition
    
    * fix: removed some testing lines and changed classifyexample type
    
    * fix: classifyexample -> example
    
    * fix: results -> classifications
    
    * fix: 5 examples per test
    
    * fix: forgot commas
    
    Co-authored-by: Callum Choi <callum@cohere.ai>

---
+++ cohere/classify.py
@@ -0,0 +1,33 @@
+from cohere.response import CohereObject
+from typing import List
+from collections import namedtuple
+
+class Confidence(CohereObject): 
+    def __init__(self, label: str, confidence: float) -> None:
+        self.label = label
+        self.confidence = confidence
+
+class Classification(CohereObject):
+    def __init__(self, input: str, prediction: str, confidence: Confidence) -> None:
+        self.input = input
+        self.prediction = prediction
+        self.confidence = confidence
+
+class Classifications(CohereObject):
+    def __init__(self, classifications: List[Classification]) -> None:
+        self.classifications = classifications
+        self.iterator = iter(classifications)
+
+    def __iter__(self) -> iter:
+        return self.iterator
+
+    def __next__(self) -> next:
+        return next(self.iterator)
+
+    def __len__(self) -> int:
+        return len(self.classifications)
+
+class Example(CohereObject):
+    def __init__(self, text: str, label: str) -> None:
+        self.text = text
+        self.label = label
--- tests/tests.py
+++ tests/tests.py
@@ -3,6 +3,7 @@ import unittest
 import cohere
 import string
 import random
+from cohere.classify import Example
 
 API_KEY = os.getenv('CO_API_KEY')
 assert type(API_KEY) != None
@@ -176,9 +177,46 @@ class TestChooseBest(unittest.TestCase):
                 options=[],
                 mode='APPEND_OPTION')
 
+class TestClassify(unittest.TestCase):
+    def test_success(self):
+        prediction = co.classify('medium', ["purple"], 
+        [Example("apple", "fruit"), Example("banana", "fruit"), Example("cherry", "fruit"), Example("watermelon", "fruit"), Example("kiwi", "fruit"), 
+        Example("red", "color"), Example("blue", "color"), Example("green", "color"), Example("yellow", "color"), Example("magenta", "color")])
+        self.assertIsInstance(prediction.classifications, list)
+        self.assertIsInstance(prediction.classifications[0].input, str)
+        self.assertIsInstance(prediction.classifications[0].prediction, str)
+        self.assertIsInstance(prediction.classifications[0].confidence.confidence, float)
+        self.assertIsInstance(prediction.classifications[0].confidence.label, str)
+        self.assertEqual(len(prediction.classifications), 1)
+        self.assertEqual(prediction.classifications[0].prediction, "color")
+
+    def test_empty_inputs(self):
+        with self.assertRaises(cohere.CohereError):
+            classifications = co.classify(
+                'medium', [], 
+                [Example("apple", "fruit"), Example("banana", "fruit"), Example("cherry", "fruit"), Example("watermelon", "fruit"), Example("kiwi", "fruit"), 
+                Example("red", "color"), Example("blue", "color"), Example("green", "color"), Example("yellow", "color"), Example("magenta", "color")])
+
+    def test_success_multi_input(self):
+        prediction = co.classify('medium', ["purple", "mango"],
+        [Example("apple", "fruit"), Example("banana", "fruit"), Example("cherry", "fruit"), Example("watermelon", "fruit"), Example("kiwi", "fruit"), 
+        Example("red", "color"), Example("blue", "color"), Example("green", "color"), Example("yellow", "color"), Example("magenta", "color")])
+        self.assertEqual(prediction.classifications[0].prediction, "color")
+        self.assertEqual(prediction.classifications[1].prediction, "fruit")
+        self.assertEqual(len(prediction.classifications), 2)
+
+    def test_success_all_fields(self):
+        prediction = co.classify('medium', ["mango", "purple"],
+        [Example("apple", "fruit"), Example("banana", "fruit"), Example("cherry", "fruit"), Example("watermelon", "fruit"), Example("kiwi", "fruit"), 
+        Example("red", "color"), Example("blue", "color"), Example("green", "color"), Example("yellow", "color"), Example("magenta", "color")], 
+        "this is a classifier to determine if a word is a fruit of a color", "This is a")
+        self.assertEqual(prediction.classifications[0].prediction, "fruit")
+        self.assertEqual(prediction.classifications[1].prediction, "color")
+
+
 class TestTokenize(unittest.TestCase):
     def test_success(self):
-        tokens = co.tokenize('large', 'tokenize me!')
+        tokens = co.tokenize('medium', 'tokenize me!')
         self.assertIsInstance(tokens.tokens, list)
         self.assertIsInstance(tokens.length, int)
         self.assertEqual(tokens.length, len(tokens))
@@ -186,7 +224,7 @@ class TestTokenize(unittest.TestCase):
     def test_invalid_text(self):
         with self.assertRaises(cohere.CohereError):
             co.tokenize(
-                model='large',
+                model='medium',
                 text='')
 
 if __name__ == '__main__':
