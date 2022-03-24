783a761110dfa0184fcfef8ea750089f9988430d
choicallum <77260697+choicallum@users.noreply.github.com>
2022-03-24 11:40:02 -0400

    v1.3.2 -> v.1.3.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='cohere',
-    version='1.3.2',
+    version='1.3.3',
     author='kipply',
     author_email='carol@cohere.ai',
     description='A Python library for the Cohere API',
