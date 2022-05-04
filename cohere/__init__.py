88052153997deeb061c2141258b482a7ba953d7b
Michael <michael.kozakov@gmail.com>
2022-05-04 13:29:00 -0400

    Sunset Choose Best (#71)

--- cohere/__init__.py
+++ cohere/__init__.py
@@ -6,7 +6,6 @@ COHERE_VERSION = '2021-11-08'
 COHERE_EMBED_BATCH_SIZE = 16
 GENERATE_URL = 'generate'
 EMBED_URL = 'embed'
-CHOOSE_BEST_URL = 'choose-best'
 CLASSIFY_URL = 'classify'
 
 CHECK_API_KEY_URL = 'check-api-key'
