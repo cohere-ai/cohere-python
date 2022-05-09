9aedc77a94aa8711c9bc831024a1771d225a6404
Michael <michael.kozakov@gmail.com>
2022-05-09 15:47:20 -0400

    Add Flake8 linter (#76)

--- cohere/tokenize.py
+++ cohere/tokenize.py
@@ -1,12 +1,13 @@
 from cohere.response import CohereObject
 from typing import List
 
+
 class Tokens(CohereObject):
     def __init__(self, tokens: List[int]) -> None:
         self.tokens = tokens
         self.iterator = iter(tokens)
         self.length = len(tokens)
-    
+
     def __iter__(self) -> iter:
         return self.iterator
 
