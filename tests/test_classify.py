55981b98c5fb9bc2e2cffe7dcac9d26f6832a134
Lucas Fayoux <8889400+lfayoux@users.noreply.github.com>
2022-07-20 16:12:25 -0400

    Change model param to use body instead of url (#84)
    * Change model param to use body instead of url

--- tests/test_classify.py
+++ tests/test_classify.py
@@ -8,18 +8,20 @@ co = cohere.Client(get_api_key())
 
 class TestClassify(unittest.TestCase):
     def test_success(self):
-        prediction = co.classify('medium', ['purple'], [
-            Example('apple', 'fruit'),
-            Example('banana', 'fruit'),
-            Example('cherry', 'fruit'),
-            Example('watermelon', 'fruit'),
-            Example('kiwi', 'fruit'),
-            Example('red', 'color'),
-            Example('blue', 'color'),
-            Example('green', 'color'),
-            Example('yellow', 'color'),
-            Example('magenta', 'color')
-        ])
+        prediction = co.classify(
+            model='medium',
+            inputs=['purple'],
+            examples=[
+                Example('apple', 'fruit'),
+                Example('banana', 'fruit'),
+                Example('cherry', 'fruit'),
+                Example('watermelon', 'fruit'),
+                Example('kiwi', 'fruit'),
+                Example('red', 'color'),
+                Example('blue', 'color'),
+                Example('green', 'color'),
+                Example('yellow', 'color'),
+                Example('magenta', 'color')])
         self.assertIsInstance(prediction.classifications, list)
         self.assertIsInstance(prediction.classifications[0].input, str)
         self.assertIsInstance(prediction.classifications[0].prediction, str)
@@ -33,7 +35,9 @@ class TestClassify(unittest.TestCase):
     def test_empty_inputs(self):
         with self.assertRaises(cohere.CohereError):
             co.classify(
-                'medium', [], [
+                model='medium',
+                inputs=[],
+                examples=[
                     Example('apple', 'fruit'),
                     Example('banana', 'fruit'),
                     Example('cherry', 'fruit'),
@@ -47,35 +51,43 @@ class TestClassify(unittest.TestCase):
                     Example('magenta', 'color')])
 
     def test_success_multi_input(self):
-        prediction = co.classify('medium', ['purple', 'mango'], [
-            Example('apple', 'fruit'),
-            Example('banana', 'fruit'),
-            Example('cherry', 'fruit'),
-            Example('watermelon', 'fruit'),
-            Example('kiwi', 'fruit'),
+        prediction = co.classify(
+            model='medium',
+            inputs=['purple', 'mango'],
+            examples=[
+                Example('apple', 'fruit'),
+                Example('banana', 'fruit'),
+                Example('cherry', 'fruit'),
+                Example('watermelon', 'fruit'),
+                Example('kiwi', 'fruit'),
 
-            Example('red', 'color'),
-            Example('blue', 'color'),
-            Example('green', 'color'),
-            Example('yellow', 'color'),
-            Example('magenta', 'color')])
+                Example('red', 'color'),
+                Example('blue', 'color'),
+                Example('green', 'color'),
+                Example('yellow', 'color'),
+                Example('magenta', 'color')])
         self.assertEqual(prediction.classifications[0].prediction, 'color')
         self.assertEqual(prediction.classifications[1].prediction, 'fruit')
         self.assertEqual(len(prediction.classifications), 2)
 
     def test_success_all_fields(self):
-        prediction = co.classify('medium', ['mango', 'purple'], [
-            Example('apple', 'fruit'),
-            Example('banana', 'fruit'),
-            Example('cherry', 'fruit'),
-            Example('watermelon', 'fruit'),
-            Example('kiwi', 'fruit'),
+        prediction = co.classify(
+            model='medium',
+            inputs=['mango', 'purple'],
+            examples=[
+                Example('apple', 'fruit'),
+                Example('banana', 'fruit'),
+                Example('cherry', 'fruit'),
+                Example('watermelon', 'fruit'),
+                Example('kiwi', 'fruit'),
 
-            Example('red', 'color'),
-            Example('blue', 'color'),
-            Example('green', 'color'),
-            Example('yellow', 'color'),
-            Example('magenta', 'color')
-        ], 'this is a classifier to determine if a word is a fruit of a color', 'This is a')
+                Example('red', 'color'),
+                Example('blue', 'color'),
+                Example('green', 'color'),
+                Example('yellow', 'color'),
+                Example('magenta', 'color')
+            ],
+            taskDescription='this is a classifier to determine if a word is a fruit of a color',
+            outputIndicator='This is a')
         self.assertEqual(prediction.classifications[0].prediction, 'fruit')
         self.assertEqual(prediction.classifications[1].prediction, 'color')
--- tests/test_tokenize.py
+++ tests/test_tokenize.py
@@ -7,11 +7,11 @@ co = cohere.Client(get_api_key())
 
 class TestTokenize(unittest.TestCase):
     def test_success(self):
-        tokens = co.tokenize('medium', 'tokenize me!')
+        tokens = co.tokenize('tokenize me!')
         self.assertIsInstance(tokens.tokens, list)
         self.assertIsInstance(tokens.length, int)
         self.assertEqual(tokens.length, len(tokens))
 
     def test_invalid_text(self):
         with self.assertRaises(cohere.CohereError):
-            co.tokenize(model='medium', text='')
+            co.tokenize(text='')
