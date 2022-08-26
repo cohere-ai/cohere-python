3d9e620ff2094651e734c1d6cc92a4d1d9f04903
Lucas Fayoux <8889400+lfayoux@users.noreply.github.com>
2022-08-26 17:11:40 -0400

    print warnings to stderr (#93)

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class BinaryDistribution(Distribution):
 
 
 setuptools.setup(name='cohere',
-                 version='2.2.2',
+                 version='2.2.3',
                  author='1vn',
                  author_email='ivan@cohere.ai',
                  description='A Python library for the Cohere API',
