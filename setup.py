5b23720cd95dbe167ff7e4ff61ece622161378c3
Michael <michael.kozakov@gmail.com>
2022-10-11 22:57:55 +0100

    Remove moderate from SDK (#104)

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class BinaryDistribution(Distribution):
 
 
 setuptools.setup(name='cohere',
-                 version='2.5.0',
+                 version='2.6.0',
                  author='1vn',
                  author_email='ivan@cohere.ai',
                  description='A Python library for the Cohere API',
