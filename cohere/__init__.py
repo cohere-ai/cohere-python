34d75a7ce68d813249c7b8525d638b53ac31afcb
Aidan Gomez <hello@aidangomez.ca>
2023-01-08 10:13:29 -0500

    add co.chat to the SDK (#132)

--- cohere/__init__.py
+++ cohere/__init__.py
@@ -4,11 +4,12 @@ from .error import CohereError
 COHERE_API_URL = 'https://api.cohere.ai'
 COHERE_VERSION = '2022-12-06'
 COHERE_EMBED_BATCH_SIZE = 16
-GENERATE_URL = 'generate'
-EMBED_URL = 'embed'
+CHAT_URL = 'chat'
 CLASSIFY_URL = 'classify'
 DETECT_LANG_URL = 'detect-language'
+EMBED_URL = 'embed'
 FEEDBACK_URL = 'feedback'
+GENERATE_URL = 'generate'
 
 CHECK_API_KEY_URL = 'check-api-key'
 TOKENIZE_URL = 'tokenize'
