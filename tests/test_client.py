6bab8f4555c06a183af05515223b849c19eae33d
Sander Land <sander@cohere.com>
2023-01-11 10:01:51 +0100

    test

---
+++ tests/test_client.py
@@ -0,0 +1,14 @@
+import unittest
+
+import cohere
+
+from utils import get_api_key
+
+API_KEY = get_api_key()
+
+
+class TestClient(unittest.TestCase):
+
+    def test_client_name(self):
+        co = cohere.Client(API_KEY, client_name='test')
+        co.generate(model='small', prompt='co:here', max_tokens=1)
