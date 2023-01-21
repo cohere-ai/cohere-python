e353dd6b1cebb9694f9bbcc97982edf8ee0b939c
Aidan Gomez <aidan@cohere.com>
2023-01-21 14:27:40 -0500

    address comments

--- cohere/feedback.py
+++ cohere/feedback.py
@@ -6,14 +6,14 @@ Example: a user accepts a model's suggestion in an assisted writing setting
 ```
 generations = co.generate(f"Write me a polite email responding to the one below:\n{email}\n\nResponse:")
 if user_accepted_suggestion:
-    generations[0].feedback(good_generation=True)
+    generations[0].feedback(good_response=True)
 ```
 
 Example: the user edits the model's suggestion
 ```
 generations = co.generate(f"Write me a polite email responding to the one below:\n{email}\n\nResponse:")
 if user_edits_suggestion:
-    generations[0].feedback(good_generation=False, desired_response=user_edited_response)
+    generations[0].feedback(good_response=False, desired_response=user_edited_response)
 ```
 """
 
