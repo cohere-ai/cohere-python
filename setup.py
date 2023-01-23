fffedaef317d28dd4da3d6de2a5dc910646a09ca
Michael <michael.kozakov@gmail.com>
2023-01-23 15:56:45 -0500

    Update embed batch size (#141)

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class BinaryDistribution(Distribution):
 
 
 setuptools.setup(name='cohere',
-                 version='3.2.0',
+                 version='3.2.1',
                  author='1vn',
                  author_email='ivan@cohere.ai',
                  description='A Python library for the Cohere API',
