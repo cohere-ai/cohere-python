ef67d0e05d4bc4243ffaf83dc8c603bcc89f853e
Michael <michael.kozakov@gmail.com>
2023-01-23 01:41:51 -0500

    Update embed batch size

--- cohere/__init__.py
+++ cohere/__init__.py
@@ -3,7 +3,7 @@ from .error import CohereError
 
 COHERE_API_URL = 'https://api.cohere.ai'
 COHERE_VERSION = '2022-12-06'
-COHERE_EMBED_BATCH_SIZE = 16
+COHERE_EMBED_BATCH_SIZE = 96
 CHAT_URL = 'chat'
 CLASSIFY_URL = 'classify'
 DETECT_LANG_URL = 'detect-language'
