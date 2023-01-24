d3aaf8fdacb6454c04370d22f17e87859f1a4567
Lucas Fayoux <lucas@cohere.com>
2023-01-24 16:47:30 -0500

    import retry from urllib3

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class BinaryDistribution(Distribution):
 
 
 setuptools.setup(name='cohere',
-                 version='3.2.1',
+                 version='3.2.2',
                  author='1vn',
                  author_email='ivan@cohere.ai',
                  description='A Python library for the Cohere API',
