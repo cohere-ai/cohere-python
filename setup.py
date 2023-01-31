541976eb924364f4a12d7e1ad26354b900bcd572
Michael Kozakov <michael.kozakov@gmail.com>
2023-01-31 17:23:00 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class BinaryDistribution(Distribution):
 
 
 setuptools.setup(name='cohere',
-                 version='3.2.3',
+                 version='3.2.4',
                  author='1vn',
                  author_email='ivan@cohere.ai',
                  description='A Python library for the Cohere API',
