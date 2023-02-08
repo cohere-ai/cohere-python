1e3c9e4edd498db8afba8536d9e9851bc4a6378f
Michael <michael.kozakov@gmail.com>
2023-02-08 09:38:04 -0500

    Fix co.summarize docstring (#153)

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ class BinaryDistribution(Distribution):
 
 
 setuptools.setup(name='cohere',
-                 version='3.3.3',
+                 version='3.3.4',
                  author='1vn',
                  author_email='ivan@cohere.ai',
                  description='A Python library for the Cohere API',
