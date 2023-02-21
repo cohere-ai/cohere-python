1403404307d49b88fe2f839f7ef23134a96c2690
Sander Land <sander@cohere.com>
2023-02-21 10:48:53 +0000

    numpy and pandas optional

--- cohere/utils.py
+++ cohere/utils.py
@@ -1,23 +1,33 @@
 import json
 
-import numpy as np
+
+try: # numpy is optional, but support json encoding if the user has it
+    import numpy as np
 
 
-class NumpyAwareJsonEncoder(json.JSONEncoder):
-    """Handles numpy datatypes and such in json encoding"""
+    class NumpyAwareJsonEncoder(json.JSONEncoder):
+        """Handles numpy datatypes and such in json encoding"""
 
-    def default(self, obj):
-        if isinstance(obj, np.integer):
-            return int(obj)
-        elif isinstance(obj, np.floating):
-            return float(obj)
-        elif isinstance(obj, np.ndarray):
-            return obj.tolist()
-        elif isinstance(obj, set):
-            return list(obj)
-        else:
-            return super().default(obj)
+        def default(self, obj):
+            if isinstance(obj, np.integer):
+                return int(obj)
+            elif isinstance(obj, np.floating):
+                return float(obj)
+            elif isinstance(obj, np.ndarray):
+                return obj.tolist()
+            elif isinstance(obj, set):
+                return list(obj)
+            else:
+                return super().default(obj)
+except:
+    class NumpyAwareJsonEncoder(json.JSONEncoder):
+        """Handles numpy datatypes and such in json encoding"""
 
+        def default(self, obj):
+            if isinstance(obj, set):
+                return list(obj)
+            else:
+                return super().default(obj)    
 
 def np_json_dumps(data, **kwargs):
     return json.dumps(data, cls=NumpyAwareJsonEncoder, **kwargs)
