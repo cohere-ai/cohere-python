1ccff76e890d554c1feb7cf7cfa506236a2dcbc6
Abdullah Elkady <abdullahkady@gmail.com>
2023-03-23 01:24:20 +0200

    Remove wrong "Embedding" class used for type hinting

--- cohere/responses/embeddings.py
+++ cohere/responses/embeddings.py
@@ -3,19 +3,8 @@ from typing import Any, Dict, Iterator, List, Optional
 from cohere.responses.base import CohereObject
 
 
-class Embedding(CohereObject):
-    def __init__(self, embedding: List[float]) -> None:
-        self.embedding = embedding
-
-    def __iter__(self) -> Iterator:
-        return iter(self.embedding)
-
-    def __len__(self) -> int:
-        return len(self.embedding)
-
-
 class Embeddings(CohereObject):
-    def __init__(self, embeddings: List[Embedding], meta: Optional[Dict[str, Any]] = None) -> None:
+    def __init__(self, embeddings: List[List[float]], meta: Optional[Dict[str, Any]] = None) -> None:
         self.embeddings = embeddings
         self.meta = meta
 
