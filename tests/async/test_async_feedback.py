e9ed75b7b44186666ef182e994846ebab62a24f9
ivan <ivan@ivanzhang.ca>
2023-04-13 14:38:24 -0400

    format

--- tests/async/test_async_feedback.py
+++ tests/async/test_async_feedback.py
@@ -10,9 +10,9 @@ from cohere.responses.feedback import (
 @pytest.mark.asyncio
 async def test_async_generate_feedback_basic(async_client):
     generations = await async_client.generate(prompt="co:here", max_tokens=1)
-    feedback = await async_client.generate_feedback(request_id=generations[0].id,
-                                                    desired_response="is the best",
-                                                    good_response=False)
+    feedback = await async_client.generate_feedback(
+        request_id=generations[0].id, desired_response="is the best", good_response=False
+    )
     assert isinstance(feedback, GenerateFeedbackResponse)
 
 
