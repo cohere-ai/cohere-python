d8fa267a1d0cae39ac16ebd2e6f6b7398325e39a
ivan <ivan@ivanzhang.ca>
2023-04-13 14:35:43 -0400

    fix async test

--- tests/async/test_async_feedback.py
+++ tests/async/test_async_feedback.py
@@ -3,15 +3,16 @@ import pytest
 from cohere.responses.feedback import (
     GenerateFeedbackResponse,
     GeneratePreferenceFeedbackResponse,
+    PreferenceRating,
 )
 
 
 @pytest.mark.asyncio
 async def test_async_generate_feedback_basic(async_client):
     generations = await async_client.generate(prompt="co:here", max_tokens=1)
-    feedback = await async_client.generate_feedback(
-        request_id=generations[0].id, desired_response="is the best", good_response=False
-    )
+    feedback = await async_client.generate_feedback(request_id=generations[0].id,
+                                                    desired_response="is the best",
+                                                    good_response=False)
     assert isinstance(feedback, GenerateFeedbackResponse)
 
 
@@ -21,8 +22,8 @@ async def test_async_generate_preference_feedback_basic(async_client):
     feedback = await async_client.generate_preference_feedback(
         prompt="co:here",
         ratings=[
-            (generations[0].id, 0.5, generations[0].generation),
-            (generations[1].id, 0.5, generations[1].generation),
+            PreferenceRating(generations[0].id, 0.5, generations[0].text),
+            PreferenceRating(generations[1].id, 0.5, generations[1].text),
         ],
     )
     assert isinstance(feedback, GeneratePreferenceFeedbackResponse)
