08cfeed0443e30d2b9b1e231f8bd030ab01d0c83
Amr Kayid <amrmkayid@gmail.com>
2023-04-18 13:14:52 -0400

    fix small typo

--- tests/sync/test_codebook.py
+++ tests/sync/test_codebook.py
@@ -15,7 +15,7 @@ class TestCodebook(unittest.TestCase):
         self.assertIsInstance(prediction.codebook[0], list)
         self.assertIsInstance(prediction.codebook[1], list)
         self.assertEqual(len(prediction.codebook[0]), 256)
-        self.assertEqual(len(prediction.codebook[1]), 8)
+        self.assertEqual(len(prediction.codebook[0][0]), 8)
         self.assertTrue(prediction.meta)
         self.assertTrue(prediction.meta["api_version"])
         self.assertTrue(prediction.meta["api_version"]["version"])
