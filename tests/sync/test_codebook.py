273c03cc435fedcc1cfdff1c989abdd30f46ee00
Amr Kayid <amrmkayid@gmail.com>
2023-04-20 13:46:31 -0400

    Update tests

--- tests/sync/test_codebook.py
+++ tests/sync/test_codebook.py
@@ -10,15 +10,25 @@ co = cohere.Client(API_KEY)
 
 class TestCodebook(unittest.TestCase):
     def test_success(self):
-        prediction = co.codebook(model="multilingual-22-12", compression_codebook="32x")
-        self.assertEqual(len(prediction.codebook), 96)
-        self.assertIsInstance(prediction.codebook[0], list)
-        self.assertIsInstance(prediction.codebook[1], list)
-        self.assertEqual(len(prediction.codebook[0]), 256)
-        self.assertEqual(len(prediction.codebook[0][0]), 8)
-        self.assertTrue(prediction.meta)
-        self.assertTrue(prediction.meta["api_version"])
-        self.assertTrue(prediction.meta["api_version"]["version"])
+        compression_codebooks = {
+            "32x": (96, 256, 8),
+            "48x": (64, 256, 12),
+            "64x": (48, 256, 16),
+            "96x": (32, 256, 24),
+        }
+        for cb, (segments, num_centroids, length) in compression_codebooks.items():
+            prediction = co.codebook(
+                model="multilingual-22-12",
+                compression_codebook=cb,
+            )
+            self.assertIsInstance(prediction.codebook[0], list)
+            self.assertIsInstance(prediction.codebook[0][0], list)
+            self.assertEqual(len(prediction.codebook), segments)
+            self.assertEqual(len(prediction.codebook[0]), num_centroids)
+            self.assertEqual(len(prediction.codebook[0][0]), length)
+            self.assertTrue(prediction.meta)
+            self.assertTrue(prediction.meta["api_version"])
+            self.assertTrue(prediction.meta["api_version"]["version"])
 
     def test_default_model(self):
         prediction = co.codebook(
@@ -26,4 +36,4 @@ class TestCodebook(unittest.TestCase):
         )
         self.assertEqual(len(prediction.codebook), 96)
         self.assertIsInstance(prediction.codebook[0], list)
-        self.assertIsInstance(prediction.codebook[1], list)
+        self.assertIsInstance(prediction.codebook[0][0], list)
