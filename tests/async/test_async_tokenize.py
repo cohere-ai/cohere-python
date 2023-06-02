2b778a0268374dbf880bb461aa334243b5027c64
Abdullah Elkady <abdullahkady@gmail.com>
2023-06-02 18:51:21 +0300

    update tests

--- tests/async/test_async_tokenize.py
+++ tests/async/test_async_tokenize.py
@@ -19,13 +19,21 @@ async def test_tokenize(async_client):
 
 @pytest.mark.asyncio
 async def test_model_param_tokenization(async_client):
-    medium_res = await async_client.tokenize("Hello world!", model="medium")
-    medium_res_batch = await async_client.batch_tokenize(["Hello world!"] * 3, model="medium")
+    # Use tuples to be hashable (for set equality)
+    medium_res = tuple((await async_client.tokenize("Hello world!", model="medium")).tokens)
+    medium_res_batch = [
+        tuple(x.tokens) for x in await async_client.batch_tokenize(["Hello world!"] * 3, model="medium")
+    ]
 
-    command_res = await async_client.tokenize("Hello world!", model="command")
-    command_res_batch = await async_client.batch_tokenize(["Hello world!"] * 3, model="command")
-    assert medium_res == set(medium_res_batch)
-    assert command_res == set(command_res_batch)
+    command_res = tuple((await async_client.tokenize("Hello world!", model="command")).tokens)
+    command_res_batch = [
+        tuple(x.tokens) for x in await async_client.batch_tokenize(["Hello world!"] * 3, model="command")
+    ]
+
+    # Assert that the result of one tokenization is the same as the result of a batch tokenization (model passed)
+    # and that the result of a tokenization with one model is different from another model
+    assert set([medium_res]) == set(medium_res_batch)
+    assert set([command_res]) == set(command_res_batch)
     assert medium_res != command_res
 
 
@@ -52,11 +60,16 @@ async def test_detokenize(async_client):
 
 @pytest.mark.asyncio
 async def test_model_param_detokenization(async_client):
-    medium_detokenized = await async_client.detokenize([10104, 12221, 974, 514, 34], model="medium")
-    medium_detokenized_batch = await async_client.batch_detokenize([[10104, 12221, 974, 514, 34]] * 3, model="medium")
+    medium_detokenized = (await async_client.detokenize([10104, 12221, 974, 514, 34], model="medium")).text
+    medium_detokenized_batch = [
+        x.text for x in await async_client.batch_detokenize([[10104, 12221, 974, 514, 34]] * 3, model="medium")
+    ]
 
-    command_detokenized = await async_client.detokenize([10104, 12221, 974, 514, 34], model="command")
-    command_detokenized_batch = await async_client.batch_detokenize([[10104, 12221, 974, 514, 34]] * 3, model="command")
-    assert medium_detokenized == set(medium_detokenized_batch)
-    assert command_detokenized == set(command_detokenized_batch)
+    command_detokenized = (await async_client.detokenize([10104, 12221, 974, 514, 34], model="command")).text
+    command_detokenized_batch = [
+        x.text for x in await async_client.batch_detokenize([[10104, 12221, 974, 514, 34]] * 3, model="command")
+    ]
+
+    assert set([medium_detokenized]) == set(medium_detokenized_batch)
+    assert set([command_detokenized]) == set(command_detokenized_batch)
     assert medium_detokenized != command_detokenized
