253cd0632b44d7988f251adfac9473f1c8d6ff6e
Raphael Cristal <raphael@cohere.com>
2023-07-11 15:10:31 +0200

    Add support for hyper parameters (#254)
    * add hyperparameters for generative custom models

--- cohere/responses/custom_model.py
+++ cohere/responses/custom_model.py
@@ -1,10 +1,11 @@
+from dataclasses import dataclass
 from datetime import datetime
 from typing import Any, Dict, Optional
 
 try:
-    from typing import Literal
+    from typing import Literal, TypedDict
 except ImportError:
-    from typing_extensions import Literal
+    from typing_extensions import Literal, TypedDict
 
 from cohere.responses.base import CohereObject
 
@@ -41,6 +42,41 @@ REVERSE_CUSTOM_MODEL_PRODUCT_MAPPING: Dict[INTERNAL_CUSTOM_MODEL_TYPE, CUSTOM_MO
 }
 
 
+@dataclass
+class HyperParameters:
+    early_stopping_patience: int
+    early_stopping_threshold: float
+    train_batch_size: int
+    train_steps: int
+    learning_rate: float
+
+    @staticmethod
+    def from_response(response: Optional[dict]) -> "HyperParameters":
+        return HyperParameters(
+            early_stopping_patience=response["earlyStoppingPatience"],
+            early_stopping_threshold=response["earlyStoppingThreshold"],
+            train_batch_size=response["trainBatchSize"],
+            train_steps=response["trainSteps"],
+            learning_rate=response["learningRate"],
+        )
+
+
+class HyperParametersInput(TypedDict):
+    """
+    early_stopping_patience: int (default=6, min=0, max=10)
+    early_stopping_threshold: float (default=0.01, min=0, max=0.1)
+    train_batch_size: int (default=16, min=2, max=16)
+    train_steps: int (default=2500, min=100, max=20000)
+    learning_rate: float (default=0.01, min=0.000005, max=0.1)
+    """
+
+    early_stopping_patience: int
+    early_stopping_threshold: float
+    train_batch_size: int
+    train_steps: int
+    learning_rate: float
+
+
 class CustomModel(CohereObject):
     def __init__(
         self,
@@ -51,6 +87,7 @@ class CustomModel(CohereObject):
         created_at: datetime,
         completed_at: Optional[datetime],
         model_id: Optional[str] = None,
+        hyperparameters: Optional[HyperParameters] = None,
     ) -> None:
         super().__init__()
         self.id = id
@@ -60,6 +97,7 @@ class CustomModel(CohereObject):
         self.created_at = created_at
         self.completed_at = completed_at
         self.model_id = model_id
+        self.hyperparameters = hyperparameters
 
     @classmethod
     def from_dict(cls, data: Dict[str, Any]) -> "CustomModel":
@@ -71,6 +109,9 @@ class CustomModel(CohereObject):
             created_at=_parse_date(data["created_at"]),
             completed_at=_parse_date(data["completed_at"]) if "completed_at" in data else None,
             model_id=data["model"]["route"] if "model" in data else None,
+            hyperparameters=HyperParameters.from_response(data["settings"]["hyperparameters"])
+            if data["settings"]["hyperparameters"]
+            else None,
         )
 
 
