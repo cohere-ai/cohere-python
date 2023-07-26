a75b4580d104b93b813cc60e294ceeba0c046aac
Lucas Fayoux <8889400+lfayoux@users.noreply.github.com>
2023-07-26 14:01:19 -0400

    Dataset beta (#268)
    * dataset beta

--- tests/async/test_async_bulk_embed.py
+++ tests/async/test_async_bulk_embed.py
@@ -3,16 +3,16 @@ import os
 import pytest
 
 from cohere import AsyncClient
-from cohere.responses.bulk_embed import BulkEmbedJob
+from cohere.responses.embed_job import EmbedJob
 
-BAD_INPUT_FILE = "./local-file.jsonl"
+BAD_DATASET_ID = "bad-id"
 IN_CI = os.getenv("CI", "").lower() in ["true", "1"]
 
 
 @pytest.mark.asyncio
 @pytest.mark.skipif(IN_CI, reason="can timeout during high load")
 async def test_create_job(async_client: AsyncClient):
-    create_res = await async_client.create_bulk_embed_job(input_file_url=BAD_INPUT_FILE)
+    create_res = await async_client.create_embed_job(input_dataset=BAD_DATASET_ID)
     job = await create_res.wait(timeout=60, interval=5)
     assert job.status == "failed"
     check_job_result(job)
@@ -20,7 +20,7 @@ async def test_create_job(async_client: AsyncClient):
 
 @pytest.mark.asyncio
 async def test_list_jobs(async_client: AsyncClient):
-    jobs = await async_client.list_bulk_embed_jobs()
+    jobs = await async_client.list_embed_jobs()
     assert len(jobs) > 0
     for job in jobs:
         check_job_result(job)
@@ -28,21 +28,20 @@ async def test_list_jobs(async_client: AsyncClient):
 
 @pytest.mark.asyncio
 async def test_get_job(async_client: AsyncClient):
-    jobs = await async_client.list_bulk_embed_jobs()
-    job = await async_client.get_bulk_embed_job(jobs[0].job_id)
+    jobs = await async_client.list_embed_jobs()
+    job = await async_client.get_embed_job(jobs[0].job_id)
     check_job_result(job)
 
 
 @pytest.mark.asyncio
 async def test_cancel_job(async_client: AsyncClient):
-    jobs = await async_client.list_bulk_embed_jobs()
-    await async_client.cancel_bulk_embed_job(jobs[0].job_id)
+    jobs = await async_client.list_embed_jobs()
+    await async_client.cancel_embed_job(jobs[0].job_id)
 
 
-def check_job_result(job: BulkEmbedJob):
+def check_job_result(job: EmbedJob):
     assert job.job_id
     assert job.status
     assert job.created_at
-    assert job.input_url
     assert job.model
     assert job.truncate
