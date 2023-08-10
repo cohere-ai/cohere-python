6805340f5b96ca0881ebddfaa0d0f738b8489540
Sander Land <sander@cohere.com>
2023-08-10 12:36:04 +0200

    edit cohere/client.py

--- cohere/client.py
+++ cohere/client.py
@@ -111,6 +111,15 @@ class Client:
         completion: Optional[str] = None,
         model: Optional[str] = None,
     ) -> LogLikelihoods:
+        """Calculates the token log-likelihood for a provided prompt and completion.
+        Using this endpoint instead of co.generate with max_tokens=0 will guarantee that any required tokens such as <EOP_TOKEN>
+        are correctly inserted, and makes it easier to retrieve only the completion log-likelihood.
+
+        Args:
+            prompt (str): The prompt
+            completion (str): The completion
+            model (str): (Optional) The model to use for calculating the log-likelihoods
+        """
         json_body = {"model": model, "prompt": prompt, "completion": completion}
         response = self._request(cohere.LOGLIKELIHOOD_URL, json=json_body)
         return LogLikelihoods(response["prompt_tokens"], response["completion_tokens"])
