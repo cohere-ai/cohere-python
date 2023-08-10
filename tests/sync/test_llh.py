908cd55ab0c7d537b2bddd1c4d65231b04c8b566
Sander Land <sander@cohere.com>
2023-08-10 12:36:59 +0200

    edit tests/sync/test_llh.py

--- tests/sync/test_llh.py
+++ tests/sync/test_llh.py
@@ -1,9 +1,14 @@
+from utils import get_api_key
+
+import cohere
 from cohere.responses import LogLikelihoods
 
 TEST_MODEL = "command-light"
+API_KEY = get_api_key()
+co = cohere.Client(API_KEY)
 
 
-async def test_basic_llh(client):
-    resp = await client.loglikelihood(model=TEST_MODEL, prompt="co:here", completion="co:where?")
+async def test_basic_llh():
+    resp = await co.loglikelihood(model=TEST_MODEL, prompt="co:here", completion="co:where?")
     assert isinstance(resp, LogLikelihoods)
     assert isinstance(resp.prompt_tokens[0].encoded, int)
