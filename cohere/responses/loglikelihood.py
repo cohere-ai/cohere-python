e198e7de3e403718de3812a4e856076399f78acc
Sander Land <sander@cohere.com>
2023-08-14 18:28:40 +0200

    edit cohere/responses/loglikelihood.py

--- cohere/responses/loglikelihood.py
+++ cohere/responses/loglikelihood.py
@@ -1,9 +1,11 @@
 from typing import Dict, List, NamedTuple, Optional
 
+from cohere.responses.base import CohereObject, _df_html
+
 TokenLogLikelihood = NamedTuple("TokenLogLikelihood", [("encoded", int), ("decoded", str), ("log_likelihood", float)])
 
 
-class LogLikelihoods:
+class LogLikelihoods(CohereObject):
     @staticmethod
     def token_list_from_dict(token_list: Optional[List[Dict]]):
         if token_list is None:
@@ -13,3 +15,25 @@ class LogLikelihoods:
     def __init__(self, prompt_tokens: List[TokenLogLikelihood], completion_tokens: List[TokenLogLikelihood]):
         self.prompt_tokens = self.token_list_from_dict(prompt_tokens)
         self.completion_tokens = self.token_list_from_dict(completion_tokens)
+
+    @property
+    def log_likelihood(self):
+        return [token.log_likelihood for token in self.completion_tokens]
+
+    def visualize(self, **kwargs):
+        import pandas as pd
+
+        html = ""
+        for lbl, tokens in [("prompt", self.prompt_tokens), ("completion", self.completion_tokens)]:
+            if tokens is not None:
+                html += f"{lbl}<br>"
+                df = pd.DataFrame.from_dict(
+                    {
+                        "decoded": [t.decoded for t in tokens],
+                        "encoded": [t.encoded for t in tokens],
+                        "log_likelihood": [t.log_likelihood for t in tokens],
+                    },
+                    orient="index",
+                )
+            html += _df_html(df.fillna(""), style={"font-size": "90%"})
+        return html
