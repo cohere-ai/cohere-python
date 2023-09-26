07a80b1c80617398c5a39a022414080a166ddc5e
Michael <michael.kozakov@gmail.com>
2023-09-26 13:45:56 +0100

    Use role instead of username in docs

--- tests/sync/test_chat.py
+++ tests/sync/test_chat.py
@@ -142,8 +142,8 @@ class TestChat(unittest.TestCase):
         prediction = co.chat(
             "Who are you?",
             chat_history=[
-                {"user_name": "User", "message": "Hey!"},
-                {"user_name": "Chatbot", "message": "Hey! How can I help you?"},
+                {"role": "User", "message": "Hey!"},
+                {"role": "Chatbot", "message": "Hey! How can I help you?"},
             ],
             return_prompt=True,
             return_chatlog=True,
