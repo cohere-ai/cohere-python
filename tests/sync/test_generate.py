b7fe9cfc19f2d6595d2ed55e2dd7c3345080df40
Lucas Fayoux <8889400+lfayoux@users.noreply.github.com>
2023-11-30 09:59:02 -0500

    update generate tests to use command-light (#352)

--- tests/sync/test_generate.py
+++ tests/sync/test_generate.py
@@ -14,7 +14,7 @@ co = cohere.Client(API_KEY)
 
 class TestGenerate(unittest.TestCase):
     def test_success(self):
-        prediction = co.generate(model="medium", prompt="co:here", max_tokens=1)
+        prediction = co.generate(model="command-light", prompt="co:here", max_tokens=1)
         self.assertIsInstance(prediction.generations[0].text, str)
         self.assertIsNone(prediction.generations[0].token_likelihoods)
         self.assertEqual(prediction.return_likelihoods, None)
@@ -24,7 +24,7 @@ class TestGenerate(unittest.TestCase):
 
     def test_success_batched(self):
         _batch_size = 10
-        predictions = co.batch_generate(model="medium", prompts=["co:here"] * _batch_size, max_tokens=1)
+        predictions = co.batch_generate(model="command-light", prompts=["co:here"] * _batch_size, max_tokens=1)
         for prediction in predictions:
             self.assertIsInstance(prediction.generations[0].text, str)
             self.assertIsNone(prediction.generations[0].token_likelihoods)
@@ -34,16 +34,16 @@ class TestGenerate(unittest.TestCase):
         prompts = ["co:here", "x y z" * 3333, "co:here"]  # too long for 8192
         # test return_exceptions = False -> fails
         with self.assertRaises(cohere.CohereError):
-            predictions = co.batch_generate(model="medium", prompts=prompts, max_tokens=1)
+            predictions = co.batch_generate(model="command-light", prompts=prompts, max_tokens=1)
         # test return_exceptions = True
-        predictions = co.batch_generate(model="medium", prompts=prompts, max_tokens=1, return_exceptions=True)
+        predictions = co.batch_generate(model="command-light", prompts=prompts, max_tokens=1, return_exceptions=True)
         self.assertEqual(len(predictions), len(prompts))
         self.assertIsInstance(predictions[1], Exception)
         self.assertIsInstance(predictions[0][0].text, str)
         self.assertIsInstance(predictions[2][0].text, str)
 
     def test_return_likelihoods_generation(self):
-        prediction = co.generate(model="medium", prompt="co:here", max_tokens=1, return_likelihoods="GENERATION")
+        prediction = co.generate(model="command-light", prompt="co:here", max_tokens=1, return_likelihoods="GENERATION")
         self.assertTrue(prediction.generations[0].token_likelihoods)
         self.assertTrue(prediction.generations[0].token_likelihoods[0].token)
         self.assertIsNotNone(prediction.generations[0].likelihood)
@@ -52,21 +52,21 @@ class TestGenerate(unittest.TestCase):
     # TODO(manoj): Fix the test expectation due to the base model change (MS-913)
     @pytest.mark.skip
     def test_return_likelihoods_all(self):
-        prediction = co.generate(model="medium", prompt="hi", max_tokens=1, return_likelihoods="ALL")
+        prediction = co.generate(model="command-light", prompt="hi", max_tokens=1, return_likelihoods="ALL")
         self.assertEqual(len(prediction.generations[0].token_likelihoods), 2)
         self.assertIsNotNone(prediction.generations[0].likelihood)
         self.assertEqual(prediction.return_likelihoods, "ALL")
 
     def test_invalid_temp(self):
         with self.assertRaises(cohere.CohereError):
-            co.generate(model="medium", prompt="hi", max_tokens=1, temperature=-1).generations
+            co.generate(model="command-light", prompt="hi", max_tokens=1, temperature=-1).generations
 
     def test_invalid_model(self):
         with self.assertRaises(cohere.CohereError):
             co.generate(model="this-better-not-exist", prompt="co:here", max_tokens=1).generations
 
     def test_no_version_works(self):
-        cohere.Client(API_KEY).generate(model="medium", prompt="co:here", max_tokens=1).generations
+        cohere.Client(API_KEY).generate(model="command-light", prompt="co:here", max_tokens=1).generations
 
     def test_invalid_key(self):
         api_key = ""
@@ -79,7 +79,7 @@ class TestGenerate(unittest.TestCase):
         self.assertIsInstance(prediction.generations[0].text, str)
 
     def test_logit_bias(self):
-        prediction = co.generate(model="medium", prompt="co:here", logit_bias={11: -5.5}, max_tokens=1)
+        prediction = co.generate(model="command-light", prompt="co:here", logit_bias={11: -5.5}, max_tokens=1)
         self.assertIsInstance(prediction.generations[0].text, str)
         self.assertIsNone(prediction.generations[0].token_likelihoods)
         self.assertEqual(prediction.return_likelihoods, None)
