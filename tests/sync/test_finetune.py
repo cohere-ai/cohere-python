ccd71da9ca2191f6acdb6f6d1f83f94801be408b
daniel <daniel@cohere.com>
2023-12-12 12:18:43 -0500

    skip flaky test

--- tests/sync/test_finetune.py
+++ tests/sync/test_finetune.py
@@ -7,6 +7,7 @@ import cohere
 API_KEY = get_api_key()
 client = cohere.Client(API_KEY)
 
+IN_CI = os.getenv("CI", "").lower() in ["true", "1"]
 
 class TestFinetuneClient(unittest.TestCase):
     def test_list(self):
@@ -17,6 +18,8 @@ class TestFinetuneClient(unittest.TestCase):
         by_id = client.get_custom_model(first.id)
         self.assertEqual(first.id, by_id.id)
 
+
+    @pytest.mark.skipif(IN_CI, reason="flaky in CI for some reason")
     def test_metrics(self):
         models = client.list_custom_models(statuses=["PAUSED", "READY"])
         # there should always be a model, but make sure tests don't randomly break
