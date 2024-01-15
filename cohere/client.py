2f74acaa86bb7e861994e23eac10b0d058368aa7
Lucas Fayoux <8889400+lfayoux@users.noreply.github.com>
2024-01-15 00:10:55 -0500

    Update cohere/client.py
    Co-authored-by: Michael <michael.kozakov@gmail.com>

--- cohere/client.py
+++ cohere/client.py
@@ -1056,7 +1056,7 @@ class Client:
         """Create embed job.
 
         Args:
-            dataset_id (str): dataset id with text to embed.
+            dataset_id (str): ID of the dataset of text to embed.
             model (str): The model ID to use for embedding the text.
             input_type (str): One of "classification", "clustering", "search_document", "search_query". The type of input text provided to embed.
             truncate (Optional[str], optional): How the API handles text longer than the maximum token length. Defaults to None.
