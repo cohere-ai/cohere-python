d87b4a4115148f71b167b1ba2770a03f262cb1f7
Billy Trend <billy@cohere.com>
2024-01-24 12:11:12 +0000

    sdk usage
    sdk usage

--- sdk-usage.py
+++ sdk-usage.py
@@ -1,21 +1,20 @@
-import cohere
-from cohere.client import ClassifyExample, ParseInfo
+from cohere.client import Client
+from cohere import GenerateRequestReturnLikelihoods, GenerateRequestTruncate, ChatMessage, ChatMessageRole, ClassifyRequestExamplesItem, ClassifyRequestTruncate, SummarizeRequestLength, SummarizeRequestFormat, SummarizeRequestExtractiveness, EmbedInputType, EmbedRequestTruncate, EmbedInputType, CreateEmbedJobRequestTruncate, ChatRequestCitationQuality, ChatRequestPromptTruncation, DatasetType, ChatConnector
 
-co = cohere.Client(
-    api_key="xxx",
-    num_workers=64,
-    request_dict={},
-    check_api_key=True,
+co = Client(
+    token="xxx",
+    # num_workers=64, not supported
+    # request_dict={}, not supported
+    # check_api_key=True, not supported
     client_name="langchain",
-    max_retries=3,
+    # max_retries=3, not supported
     timeout=120,
-    api_url="https://api.cohere.com",
+    base_url="https://api.cohere.com",
 )
 
 prediction = co.generate(
-    stream=False, # please see the streaming section
     prompt="count with me!",
-    prompt_vars={"count": 1},
+    # prompt_vars={"count": 1}, not supported
     model="command",
     preset="id",
     num_generations=1,
@@ -27,46 +26,44 @@ prediction = co.generate(
     presence_penalty=1,
     end_sequences=["\n"],
     stop_sequences=["\n"],
-    return_likelihoods="ALL",
-    truncate="END",
-    logit_bias={1: 1},
+    return_likelihoods=GenerateRequestReturnLikelihoods.NONE,
+    truncate=GenerateRequestTruncate.END,
+    logit_bias={"1": 1},
 )
 
 chat = co.chat(
-    stream=False,
     message="2",
     model="command",
-    return_chat_history=True,
-    return_prompt=True,
-    return_preamble=True,
+    # return_chat_history=True, not supported
+    # return_prompt=True, not supported
+    # return_preamble=True, not supported
     chat_history=[
-        {"role": "User", "message": "Count with me!"},
-        {"role": "User", "message": "1"}
+        ChatMessage(role=ChatMessageRole.USER, message="Count with me!"),
+        ChatMessage(role=ChatMessageRole.USER, message="1")
     ],
-    preamble_override=None,
-    user_name=None,
+    preamble_override="Preamble!!",
+    # user_name=None,
     temperature=0.8,
-    max_tokens=None,
-    p=None,
-    k=None,
-    logit_bias=None,
-    search_queries_only=True,
+    # max_tokens=None, not supported
+    # p=None, not supported
+    # k=None, not supported
+    # logit_bias=None, not supported
+    search_queries_only=False,
     documents=[
         {
             "id": "1",
             "text": "The quick brown fox jumped over the lazy dog.",
         }
     ],
-    citation_quality="ACCURATE",
-    prompt_truncation="AUTO",
+    citation_quality=ChatRequestCitationQuality.ACCURATE,
+    prompt_truncation=ChatRequestPromptTruncation.AUTO,
     connectors=[
-        {
-            "id": "web-search",
-            "id": "web-search",
-            "user_access_token": "xxx",
-            "continue_on_failure": False,
-            "options": {"site": "cohere.com"}
-        }
+        ChatConnector(
+            id="web-search",
+            user_access_token="xxx",
+            continue_on_failure=False,
+            options={"site": "cohere.com"}
+        )
     ],
 )
 
@@ -74,17 +71,17 @@ print('chat: {}'.format(chat.text))
 
 classifies = co.classify(
     examples=[
-        ClassifyExample(text="orange", label="fruit"),
-        ClassifyExample(text="pear", label="fruit"),
-        ClassifyExample(text="lettuce", label="vegetable"),
-        ClassifyExample(text="cauliflower", label="vegetable")
+        ClassifyRequestExamplesItem(text="orange", label="fruit"),
+        ClassifyRequestExamplesItem(text="pear", label="fruit"),
+        ClassifyRequestExamplesItem(text="lettuce", label="vegetable"),
+        ClassifyRequestExamplesItem(text="cauliflower", label="vegetable"),
     ],
     inputs=[
         "Abiu",
     ],
     model="embed-multilingual-v2.0",
     preset="id",
-    truncate="END",
+    truncate=ClassifyRequestTruncate.END,
 )
 
 print('classifies: {}'.format(classifies.classifications[0].prediction))
@@ -106,11 +103,11 @@ print('detokenise: {}'.format(detokenise.text))
 summarise = co.summarize(
     text="the quick brown fox jumped over the lazy dog and then the dog jumped over the fox the quick brown fox jumped over the lazy dog the quick brown fox jumped over the lazy dog the quick brown fox jumped over the lazy dog the quick brown fox jumped over the lazy dog",
     model="command",
-    length="short",
-    format="paragraph",
+    length=SummarizeRequestLength.SHORT,
+    format=SummarizeRequestFormat.PARAGRAPH,
     temperature=1,
     additional_command=None,
-    extractiveness="low",
+    extractiveness=SummarizeRequestExtractiveness.LOW,
 )
 
 print('summarise: {}'.format(summarise))
@@ -133,26 +130,26 @@ print('rerank: {}'.format(rerank.results[0].index))
 embed = co.embed(
     texts=['hello', 'goodbye'],
     model='embed-english-v3.0',
-    truncate='NONE',
-    input_type='search_document',
+    truncate=EmbedRequestTruncate.NONE,
+    input_type=EmbedInputType.SEARCH_DOCUMENT,
     embedding_types=['uint8'],
 )
 
 print(embed)
 
-my_dataset = co.create_dataset(
+my_dataset = co.datasets.create(
     name="prompt-completion-dataset",
     data=open("./dataset.jsonl", "rb"),
-    dataset_type="embed-input",
+    type=DatasetType.EMBED_INPUT,
     # eval_data=open("./prompt-completion.jsonl", "rb"),
     keep_fields="all",
     optional_fields="all",
-    parse_info=ParseInfo(separator="\n", delimiter=","),
+    # parse_info=ParseInfo(separator="\n", delimiter=","), not supported
 )
 
 print(my_dataset)
 
-my_datasets = co.list_datasets(
+my_datasets = co.datasets.list(
     dataset_type="embed",
     limit=10,
     offset=0,
@@ -160,47 +157,47 @@ my_datasets = co.list_datasets(
 
 print(my_datasets)
 
-dataset_usage = co.get_dataset_usage()
+dataset_usage = co.datasets.get_usage()
 
 print(dataset_usage)
 
-my_dataset = co.get_dataset(
+my_dataset = co.datasets.get(
     my_dataset.id
 )
 
 print(my_dataset)
 
-co.delete_dataset(
+co.datasets.delete(
     my_dataset.id
 )
 
 # start an embed job
-job = co.create_embed_job(
+job = co.embed_jobs.create(
     dataset_id=my_dataset.id,
-    input_type='search_document',
+    input_type=EmbedInputType.SEARCH_DOCUMENT,
     model='embed-english-v3.0',
-    truncate='END',
+    truncate=CreateEmbedJobRequestTruncate.END,
     name='my embed job',
 )
 
 print(job)
 
-my_embed_jobs = co.list_embed_jobs()
+my_embed_jobs = co.embed_jobs.list()
 
 print(my_embed_jobs)
 
-my_embed_job = co.get_embed_job(
+my_embed_job = co.embed_jobs.get(
     job.id
 )
 
 print(my_embed_job)
 
 # cancel an embed job
-co.cancel_embed_job(
+co.embed_jobs.cancel(
     my_embed_job.id
 )
 
-created_connector = co.create_connector(
+created_connector = co.connectors.create(
     name="Example connector",
     url="http://connector-example.com/search",
     active=True,
@@ -212,7 +209,7 @@ created_connector = co.create_connector(
 
 print(created_connector)
 
-updated_connector = co.update_connector(
+updated_connector = co.connectors.update(
     id=created_connector.id,
     name="Example connector",
     url="http://connector-example.com/search",
@@ -225,24 +222,24 @@ updated_connector = co.update_connector(
 
 print(updated_connector)
 
-connector = co.get_connector(
+connector = co.connectors.get(
     id=created_connector.id
 )
 
 print(connector)
 
-connectors = co.list_connectors(
+connectors = co.connectors.list(
     limit=10,
     offset=0,
 )
 
 print(connectors)
 
-co.delete_connector(
+co.connectors.delete(
     id=created_connector.id
 )
 
-redirect_url = co.oauth_authorize_connector(
+redirect_url = co.connectors.o_auth_authorize(
     id=created_connector.id,
     after_token_redirect="https://test.com"
 )
