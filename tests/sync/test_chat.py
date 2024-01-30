e3f42d0d5be795c02b7ac6d078d879d19ed187fa
harry-cohere <127103098+harry-cohere@users.noreply.github.com>
2024-01-30 10:13:56 -0500

    Remove certain tests (#375)

--- tests/sync/test_chat.py
+++ tests/sync/test_chat.py
@@ -3,14 +3,6 @@ import unittest
 from utils import get_api_key
 
 import cohere
-from cohere.responses.chat import (
-    StreamCitationGeneration,
-    StreamEnd,
-    StreamQueryGeneration,
-    StreamSearchResults,
-    StreamStart,
-    StreamTextGeneration,
-)
 
 API_KEY = get_api_key()
 co = cohere.Client(API_KEY)
@@ -281,73 +273,3 @@ class TestChat(unittest.TestCase):
         self.assertGreater(len(prediction.citations[0]["document_ids"]), 0)
         self.assertIsInstance(prediction.documents, list)
         self.assertGreater(len(prediction.documents), 0)
-
-    def test_stream_with_connectors(self):
-        prediction = co.chat(
-            "How deep in the Mariana Trench",
-            temperature=0,
-            stream=True,
-            connectors=[{"id": "web-search"}],
-            prompt_truncation="AUTO",
-        )
-
-        self.assertIsInstance(prediction, cohere.responses.chat.StreamingChat)
-        self.assertIsInstance(prediction.texts, list)
-        self.assertEqual(len(prediction.texts), 0)
-        self.assertIsNone(prediction.response_id)
-        self.assertIsNone(prediction.finish_reason)
-
-        expected_index = 0
-        expected_text = ""
-
-        count_stream_start = 0
-        count_text_generation = 0
-        count_query_generation = 0
-        count_citation_generation = 0
-        count_search_results = 0
-        count_stream_end = 0
-        for token in prediction:
-            if isinstance(token, StreamStart):
-                count_stream_start += 1
-                self.assertIsNotNone(token.generation_id)
-                self.assertFalse(token.is_finished)
-                self.assertEqual(token.event_type, "stream-start")
-            elif isinstance(token, StreamQueryGeneration):
-                count_query_generation += 1
-                self.assertIsNotNone(token.search_queries)
-                self.assertEqual(token.event_type, "search-queries-generation")
-            elif isinstance(token, StreamSearchResults):
-                count_search_results += 1
-                self.assertIsNotNone(token.documents)
-                self.assertIsNotNone(token.search_results)
-                self.assertEqual(token.event_type, "search-results")
-            elif isinstance(token, StreamCitationGeneration):
-                count_citation_generation += 1
-                self.assertIsNotNone(token.citations)
-                self.assertEqual(token.event_type, "citation-generation")
-            elif isinstance(token, StreamTextGeneration):
-                count_text_generation += 1
-                self.assertIsInstance(token.text, str)
-                self.assertGreater(len(token.text), 0)
-                expected_text += token.text
-                self.assertFalse(token.is_finished)
-                self.assertEqual(token.event_type, "text-generation")
-            elif isinstance(token, StreamEnd):
-                count_stream_end += 1
-                self.assertTrue(token.is_finished)
-                self.assertEqual(token.event_type, "stream-end")
-                self.assertEqual(token.finish_reason, "COMPLETE")
-            self.assertIsInstance(token.index, int)
-            self.assertEqual(token.index, expected_index)
-            expected_index += 1
-
-        self.assertEqual(count_stream_start, 1)
-        self.assertEqual(count_search_results, 1)
-        self.assertEqual(count_stream_end, 1)
-        self.assertGreaterEqual(count_citation_generation, 1)
-        self.assertGreaterEqual(count_query_generation, 1)
-        self.assertGreaterEqual(count_text_generation, 1)
-
-        self.assertEqual(prediction.texts, [expected_text])
-        self.assertIsNotNone(prediction.response_id)
-        self.assertIsNotNone(prediction.finish_reason)
