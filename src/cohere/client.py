c06eb390c3de39f4507d53429c0e9b272f7fbbc7
Billy Trend <billy@cohere.com>
2024-02-22 11:05:18 -0500

    Restore

---
+++ src/cohere/client.py
@@ -0,0 +1,28 @@
+import typing
+
+import httpx
+
+from . import BaseCohereEnvironment
+from .base_client import BaseCohere
+
+
+class Client(BaseCohere):
+    def __init__(
+            self,
+            api_key: typing.Union[str, typing.Callable[[], str]],
+            *,
+            base_url: typing.Optional[str] = None,
+            environment: BaseCohereEnvironment = BaseCohereEnvironment.PRODUCTION,
+            client_name: typing.Optional[str] = None,
+            timeout: typing.Optional[float] = 60,
+            httpx_client: typing.Optional[httpx.Client] = None,
+    ):
+        BaseCohere.__init__(
+            self,
+            base_url=base_url,
+            environment=environment,
+            client_name=client_name,
+            token=api_key,
+            timeout=timeout,
+            httpx_client=httpx_client,
+        )
