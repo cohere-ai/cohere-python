73d13e21297de3f3fa434084160363b82a29c699
billytrend-cohere <144115527+billytrend-cohere@users.noreply.github.com>
2024-03-27 10:26:32 -0500

    Add support for batching in .embed(...) (#437)
    * Add support for batching in .embed(...)
    
    * merge testing
    
    * Fix mypy
    
    * Fix mypy
    
    * Fix name
    
    * .fernignore

--- src/cohere/utils.py
+++ src/cohere/utils.py
@@ -3,7 +3,8 @@ import time
 import typing
 from typing import Optional
 
-from .types import EmbedJob, CreateEmbedJobResponse
+from . import EmbedResponse, EmbedResponse_EmbeddingsFloats, EmbedResponse_EmbeddingsByType, ApiMeta, \
+    EmbedByTypeResponseEmbeddings, ApiMetaBilledUnits, EmbedJob, CreateEmbedJobResponse
 from .datasets import DatasetsCreateResponse, DatasetsGetResponse
 
 
@@ -31,8 +32,8 @@ def get_validation_status(awaitable: typing.Union[EmbedJob, DatasetsGetResponse]
 
 def get_job(cohere: typing.Any,
             awaitable: typing.Union[CreateEmbedJobResponse, DatasetsCreateResponse, EmbedJob, DatasetsGetResponse]) -> \
-typing.Union[
-    EmbedJob, DatasetsGetResponse]:
+        typing.Union[
+            EmbedJob, DatasetsGetResponse]:
     if awaitable.__class__.__name__ == "EmbedJob" or awaitable.__class__.__name__ == "CreateEmbedJobResponse":
         return cohere.embed_jobs.get(id=get_id(awaitable))
     elif awaitable.__class__.__name__ == "DatasetsGetResponse" or awaitable.__class__.__name__ == "DatasetsCreateResponse":
@@ -150,3 +151,81 @@ async def async_wait(
         raise Exception(get_failure_reason(job))
 
     return job
+
+
+def sum_fields_if_not_none(obj: typing.Any, field: str) -> Optional[int]:
+    non_none = [getattr(obj, field) for obj in obj if getattr(obj, field) is not None]
+    return sum(non_none) if non_none else None
+
+
+def merge_meta_field(metas: typing.List[ApiMeta]) -> ApiMeta:
+    api_version = metas[0].api_version
+    billed_units = [meta.billed_units for meta in metas]
+    input_tokens = sum_fields_if_not_none(billed_units, "input_tokens")
+    output_tokens = sum_fields_if_not_none(billed_units, "output_tokens")
+    search_units = sum_fields_if_not_none(billed_units, "search_units")
+    classifications = sum_fields_if_not_none(billed_units, "classifications")
+    warnings = {warning for meta in metas if meta.warnings for warning in meta.warnings}
+    return ApiMeta(
+        api_version=api_version,
+        billed_units=ApiMetaBilledUnits(
+            input_tokens=input_tokens,
+            output_tokens=output_tokens,
+            search_units=search_units,
+            classifications=classifications
+        ),
+        warnings=list(warnings)
+    )
+
+
+def merge_embed_responses(responses: typing.List[EmbedResponse]) -> EmbedResponse:
+    meta = merge_meta_field([response.meta for response in responses if response.meta])
+    response_id = ", ".join(response.id for response in responses)
+    texts = [
+        text
+        for response in responses
+        for text in response.texts
+    ]
+
+    if responses[0].response_type == "embeddings_floats":
+        embeddings_floats = typing.cast(typing.List[EmbedResponse_EmbeddingsFloats], responses)
+
+        embeddings = [
+            embedding
+            for embeddings_floats in embeddings_floats
+            for embedding in embeddings_floats.embeddings
+        ]
+
+        return EmbedResponse_EmbeddingsFloats(
+            response_type="embeddings_floats",
+            id=response_id,
+            texts=texts,
+            embeddings=embeddings,
+            meta=meta
+        )
+    else:
+        embeddings_type = typing.cast(typing.List[EmbedResponse_EmbeddingsByType], responses)
+
+        embeddings_by_type = [
+            response.embeddings
+            for response in embeddings_type
+        ]
+
+        merged_dicts = {
+            field: [
+                embedding
+                for embedding_by_type in embeddings_by_type
+                for embedding in getattr(embedding_by_type, field)
+            ]
+            for field in EmbedByTypeResponseEmbeddings.__fields__
+        }
+
+        embeddings_by_type_merged = EmbedByTypeResponseEmbeddings.parse_obj(merged_dicts)
+
+        return EmbedResponse_EmbeddingsByType(
+            response_type="embeddings_by_type",
+            id=response_id,
+            embeddings=embeddings_by_type_merged,
+            texts=texts,
+            meta=meta
+        )
---
+++ tests/test_embed_utils.py
@@ -0,0 +1,149 @@
+import unittest
+
+from cohere import EmbedResponse_EmbeddingsByType, EmbedByTypeResponseEmbeddings, ApiMeta, ApiMetaBilledUnits, \
+    ApiMetaApiVersion, EmbedResponse_EmbeddingsFloats
+from cohere.utils import merge_embed_responses
+
+ebt_1 = EmbedResponse_EmbeddingsByType(
+    response_type="embeddings_by_type",
+    id="1",
+    embeddings=EmbedByTypeResponseEmbeddings(
+        float=[[0, 1, 2], [3, 4, 5]],
+        int8=[[0, 1, 2], [3, 4, 5]],
+        uint8=[[0, 1, 2], [3, 4, 5]],
+        binary=[[0, 1, 2], [3, 4, 5]],
+        ubinary=[[0, 1, 2], [3, 4, 5]],
+    ),
+    texts=["hello", "goodbye"],
+    meta=ApiMeta(
+        api_version=ApiMetaApiVersion(version="1"),
+        billed_units=ApiMetaBilledUnits(
+            input_tokens=1,
+            output_tokens=1,
+            search_units=1,
+            classifications=1
+        ),
+        warnings=["test_warning_1"]
+    )
+)
+
+ebt_2 = EmbedResponse_EmbeddingsByType(
+    response_type="embeddings_by_type",
+    id="2",
+    embeddings=EmbedByTypeResponseEmbeddings(
+        float=[[7, 8, 9], [10, 11, 12]],
+        int8=[[7, 8, 9], [10, 11, 12]],
+        uint8=[[7, 8, 9], [10, 11, 12]],
+        binary=[[7, 8, 9], [10, 11, 12]],
+        ubinary=[[7, 8, 9], [10, 11, 12]],
+    ),
+    texts=["bye", "seeya"],
+    meta=ApiMeta(
+        api_version=ApiMetaApiVersion(version="1"),
+        billed_units=ApiMetaBilledUnits(
+            input_tokens=2,
+            output_tokens=2,
+            search_units=2,
+            classifications=2
+        ),
+        warnings=["test_warning_1", "test_warning_2"]
+    )
+)
+
+ebf_1 = EmbedResponse_EmbeddingsFloats(
+    response_type="embeddings_floats",
+    id="1",
+    texts=["hello", "goodbye"],
+    embeddings=[[0, 1, 2], [3, 4, 5]],
+    meta=ApiMeta(
+        api_version=ApiMetaApiVersion(version="1"),
+        billed_units=ApiMetaBilledUnits(
+            input_tokens=1,
+            output_tokens=1,
+            search_units=1,
+            classifications=1
+        ),
+        warnings=["test_warning_1"]
+    )
+)
+
+ebf_2 = EmbedResponse_EmbeddingsFloats(
+    response_type="embeddings_floats",
+    id="2",
+    texts=["bye", "seeya"],
+    embeddings=[[7, 8, 9], [10, 11, 12]],
+    meta=ApiMeta(
+        api_version=ApiMetaApiVersion(version="1"),
+        billed_units=ApiMetaBilledUnits(
+            input_tokens=2,
+            output_tokens=2,
+            search_units=2,
+            classifications=2
+        ),
+        warnings=["test_warning_1", "test_warning_2"]
+    )
+)
+
+
+class TestClient(unittest.TestCase):
+
+    def test_merge_embeddings_by_type(self) -> None:
+        resp = merge_embed_responses([
+            ebt_1,
+            ebt_2
+        ])
+
+
+        if resp.meta is None:
+            raise Exception("this is just for mpy")
+
+        self.assertEqual(set(resp.meta.warnings or []), {"test_warning_1", "test_warning_2"})
+        self.assertEqual(resp, EmbedResponse_EmbeddingsByType(
+            response_type="embeddings_by_type",
+            id="1, 2",
+            embeddings=EmbedByTypeResponseEmbeddings(
+                float=[[0, 1, 2], [3, 4, 5], [7, 8, 9], [10, 11, 12]],
+                int8=[[0, 1, 2], [3, 4, 5], [7, 8, 9], [10, 11, 12]],
+                uint8=[[0, 1, 2], [3, 4, 5], [7, 8, 9], [10, 11, 12]],
+                binary=[[0, 1, 2], [3, 4, 5], [7, 8, 9], [10, 11, 12]],
+                ubinary=[[0, 1, 2], [3, 4, 5], [7, 8, 9], [10, 11, 12]],
+            ),
+            texts=["hello", "goodbye", "bye", "seeya"],
+            meta=ApiMeta(
+                api_version=ApiMetaApiVersion(version="1"),
+                billed_units=ApiMetaBilledUnits(
+                    input_tokens=3,
+                    output_tokens=3,
+                    search_units=3,
+                    classifications=3
+                ),
+                warnings=resp.meta.warnings  # order ignored
+            )
+        ))
+
+    def test_merge_embeddings_floats(self) -> None:
+        resp = merge_embed_responses([
+            ebf_1,
+            ebf_2
+        ])
+
+        if resp.meta is None:
+            raise Exception("this is just for mpy")
+
+        self.assertEqual(set(resp.meta.warnings or []), {"test_warning_1", "test_warning_2"})
+        self.assertEqual(resp, EmbedResponse_EmbeddingsFloats(
+            response_type="embeddings_floats",
+            id="1, 2",
+            texts=["hello", "goodbye", "bye", "seeya"],
+            embeddings=[[0, 1, 2], [3, 4, 5], [7, 8, 9], [10, 11, 12]],
+            meta=ApiMeta(
+                api_version=ApiMetaApiVersion(version="1"),
+                billed_units=ApiMetaBilledUnits(
+                    input_tokens=3,
+                    output_tokens=3,
+                    search_units=3,
+                    classifications=3
+                ),
+                warnings=resp.meta.warnings  # order ignored
+            )
+        ))
