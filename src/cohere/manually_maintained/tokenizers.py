2c8ed2f7b819108e08459d0548fa1a71d393e46f
Abdullah Elkady <abdullah@cohere.com>
2024-04-01 15:49:37 +0200

    fix casting

--- src/cohere/manually_maintained/tokenizers.py
+++ src/cohere/manually_maintained/tokenizers.py
@@ -27,10 +27,10 @@ async def get_hf_tokenizer(co: typing.Union["AsyncClient", "Client"], model: str
     if not response.tokenizer_url:
         raise ValueError(f"No tokenizer URL found for model {model}")
 
-    size = typing.cast(int, requests.head(response.tokenizer_url).headers.get("Content-Length"))
+    size = int(typing.cast(int, requests.head(response.tokenizer_url).headers.get("Content-Length")))
     size_mb = round(size / 1024 / 1024, 2)
     print(f"Downloading tokenizer for model {model}. Size is {size_mb} MBs.")
-    resource = requests.get(response.tokenizer_url)
+    resource = requests.get(response.tokenizer_url)  # TODO: make this async compatible
     tokenizer = Tokenizer.from_str(resource.text)
 
     co._cache_set(tokenizer_cache_key(model), tokenizer)
