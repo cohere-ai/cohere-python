c0bcb77d3fa1a897bc8983f0afb55cf1fdef3c3c
billytrend-cohere <144115527+billytrend-cohere@users.noreply.github.com>
2024-04-09 06:30:29 -0500

    Revert "Drop skipIf (#468)"
    This reverts commit 5e83af3dcc489667d6ce8632ba8b8e3158e7d8b5.

--- tests/test_async_client.py
+++ tests/test_async_client.py
@@ -68,12 +68,14 @@ class TestClient(unittest.IsolatedAsyncioTestCase):
         with self.assertRaises(ValueError):
             await self.co.list_connectors("dummy", dummy="dummy")  # type: ignore
 
+    @unittest.skipIf(os.getenv("CO_API_URL") is not None, "Doesn't work in staging.")
     async def test_generate(self) -> None:
         response = await self.co.generate(
             prompt='Please explain to me how LLMs work',
         )
         print(response)
 
+    @unittest.skipIf(os.getenv("CO_API_URL") is not None, "Doesn't work in staging.")
     async def test_embed(self) -> None:
         response = await self.co.embed(
             texts=['hello', 'goodbye'],
@@ -119,6 +121,7 @@ class TestClient(unittest.IsolatedAsyncioTestCase):
         print(response)
 
 
+    @unittest.skipIf(os.getenv("CO_API_URL") is not None, "Doesn't work in staging.")
     async def test_embed_job_crud(self) -> None:
         dataset = await self.co.datasets.create(
             name="test",
@@ -167,6 +170,7 @@ class TestClient(unittest.IsolatedAsyncioTestCase):
 
         print(response)
 
+    @unittest.skipIf(os.getenv("CO_API_URL") is not None, "Doesn't work in staging.")
     async def test_classify(self) -> None:
         examples = [
             ClassifyExample(text="Dermatologists don't like her!", label="Spam"),
@@ -194,6 +198,7 @@ class TestClient(unittest.IsolatedAsyncioTestCase):
         )
         print(response)
 
+    @unittest.skipIf(os.getenv("CO_API_URL") is not None, "Doesn't work in staging.")
     async def test_datasets_crud(self) -> None:
         my_dataset = await self.co.datasets.create(
             name="test",
@@ -213,6 +218,7 @@ class TestClient(unittest.IsolatedAsyncioTestCase):
 
         await self.co.datasets.delete(my_dataset.id or "")
 
+    @unittest.skipIf(os.getenv("CO_API_URL") is not None, "Doesn't work in staging.")
     async def test_save_load(self) -> None:
         my_dataset = await self.co.datasets.create(
             name="test",
@@ -278,6 +284,7 @@ class TestClient(unittest.IsolatedAsyncioTestCase):
         )
         print(response)
 
+    @unittest.skipIf(os.getenv("CO_API_URL") is not None, "Doesn't work in staging.")
     async def test_connectors_crud(self) -> None:
         created_connector = await self.co.connectors.create(
             name="Example connector",
@@ -300,6 +307,7 @@ class TestClient(unittest.IsolatedAsyncioTestCase):
 
         await self.co.connectors.delete(created_connector.connector.id)
 
+    @unittest.skipIf(os.getenv("CO_API_URL") is not None, "Doesn't work in staging.")
     async def test_tool_use(self) -> None:
         tools = [
             Tool(
