5b67ca483c525312beebf2c6ea5292fa19fe4883
Billy Trend <billy@cohere.com>
2024-05-09 11:32:20 +0100

    Update tests

--- tests/test_bedrock_client.py
+++ tests/test_bedrock_client.py
@@ -15,7 +15,7 @@ package_dir = os.path.dirname(os.path.abspath(__file__))
 embed_job = os.path.join(package_dir, 'embed_job.jsonl')
 
 
-@unittest.skip("Skip test")
+@unittest.skip("Skip test in ci until auth is set up")
 class TestClient(unittest.TestCase):
     def test_embed(self) -> None:
         response = co.embed(
@@ -44,10 +44,25 @@ class TestClient(unittest.TestCase):
         )
         print(response)
 
+        self.assertIsNotNone(response.text)
+        self.assertIsNotNone(response.generation_id)
+        self.assertIsNotNone(response.finish_reason)
+
     def test_chat_stream(self) -> None:
+        response_types = set()
         response = co.chat_stream(
             message='Please explain to me how LLMs work',
         )
         for event in response:
+            response_types.add(event.event_type)
             if event.event_type == "text-generation":
                 print(event.text, end='')
+                self.assertIsNotNone(event.text)
+            if event.event_type == "stream-end":
+                self.assertIsNotNone(event.finish_reason)
+                self.assertIsNotNone(event.response)
+                self.assertIsNotNone(event.is_finished)
+                self.assertIsNotNone(event.response.text)
+                self.assertIsNotNone(event.response.response_id)
+
+        self.assertSetEqual(response_types, {"text-generation", "stream-end"})
