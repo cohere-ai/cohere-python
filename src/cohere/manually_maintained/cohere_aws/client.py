efd27a7074efe2ae70b73b260c3b2a43a7ebbca1
billytrend-cohere <144115527+billytrend-cohere@users.noreply.github.com>
2024-10-30 17:11:24 +0000

    Fix sagemaker client (#596)
    * Update client.py
    
    * Update client.py

--- src/cohere/manually_maintained/cohere_aws/client.py
+++ src/cohere/manually_maintained/cohere_aws/client.py
@@ -39,7 +39,8 @@ class Client:
         """
         if not AWS_DEPS_AVAILABLE:
             raise CohereError("AWS dependencies not available. Please install boto3 and sagemaker.")
-        self._client = boto3.client()
+        self._client = boto3.client("sagemaker-runtime", region_name=aws_region)
+        self._service_client = boto3.client("sagemaker", region_name=aws_region)
         if os.environ.get('AWS_DEFAULT_REGION') is None:
             os.environ['AWS_DEFAULT_REGION'] = aws_region
         self._sess = sage.Session(sagemaker_client=self._service_client)
