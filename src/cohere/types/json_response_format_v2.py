cea78cf35b9fc230ae23b7a75c2a365fe791ab31
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-11-15 20:29:40 +0000

    SDK regeneration

--- src/cohere/types/json_response_format_v2.py
+++ src/cohere/types/json_response_format_v2.py
@@ -9,7 +9,7 @@ from ..core.pydantic_utilities import IS_PYDANTIC_V2
 class JsonResponseFormatV2(UncheckedBaseModel):
     json_schema: typing.Optional[typing.Dict[str, typing.Optional[typing.Any]]] = pydantic.Field(default=None)
     """
-    A [JSON schema](https://json-schema.org/overview/what-is-jsonschema) object that the output will adhere to. There are some restrictions we have on the schema, refer to [our guide](/docs/structured-outputs-json#schema-constraints) for more information.
+    A [JSON schema](https://json-schema.org/overview/what-is-jsonschema) object that the output will adhere to. There are some restrictions we have on the schema, refer to [our guide](https://docs.cohere.com/docs/structured-outputs-json#schema-constraints) for more information.
     Example (required name and age object):
     
     ```json
