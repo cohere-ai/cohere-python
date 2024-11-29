245949d9f7675d7b3c51e4122fc665861b6ef5ce
Billy Trend <billy@cohere.com>
2024-11-29 12:48:15 +0000

    Add v2 clients

--- src/cohere/sagemaker_client.py
+++ src/cohere/sagemaker_client.py
@@ -1,6 +1,6 @@
 import typing
 
-from .aws_client import AwsClient
+from .aws_client import AwsClient, AwsClientV2
 from .manually_maintained.cohere_aws.client import Client
 from .manually_maintained.cohere_aws.mode import Mode
 
@@ -18,6 +18,30 @@ class SagemakerClient(AwsClient):
             timeout: typing.Optional[float] = None,
     ):
         AwsClient.__init__(
+            self,
+            service="sagemaker",
+            aws_access_key=aws_access_key,
+            aws_secret_key=aws_secret_key,
+            aws_session_token=aws_session_token,
+            aws_region=aws_region,
+            timeout=timeout,
+        )
+        self.sagemaker_finetuning = Client(aws_region=aws_region)
+
+
+class SagemakerClientV2(AwsClientV2):
+    sagemaker_finetuning: Client
+
+    def __init__(
+            self,
+            *,
+            aws_access_key: typing.Optional[str] = None,
+            aws_secret_key: typing.Optional[str] = None,
+            aws_session_token: typing.Optional[str] = None,
+            aws_region: typing.Optional[str] = None,
+            timeout: typing.Optional[float] = None,
+    ):
+        AwsClientV2.__init__(
             self,
             service="sagemaker",
             aws_access_key=aws_access_key,
