704cfacc9450953a230d5175433893e5ef16175e
Andrew Berneshawi <andrewberneshawi@cohere.com>
2024-12-04 20:13:53 -0800

    Revert "Add bedrock test and v2 clis (#609)"
    This reverts commit bd906fa4828eb365f167b197342c9c49b50392aa.

--- src/cohere/bedrock_client.py
+++ src/cohere/bedrock_client.py
@@ -2,7 +2,7 @@ import typing
 
 from tokenizers import Tokenizer  # type: ignore
 
-from .aws_client import AwsClient, AwsClientV2
+from .aws_client import AwsClient
 
 
 class BedrockClient(AwsClient):
@@ -16,27 +16,6 @@ class BedrockClient(AwsClient):
             timeout: typing.Optional[float] = None,
     ):
         AwsClient.__init__(
-            self,
-            service="bedrock",
-            aws_access_key=aws_access_key,
-            aws_secret_key=aws_secret_key,
-            aws_session_token=aws_session_token,
-            aws_region=aws_region,
-            timeout=timeout,
-        )
-
-
-class BedrockClientV2(AwsClientV2):
-    def __init__(
-            self,
-            *,
-            aws_access_key: typing.Optional[str] = None,
-            aws_secret_key: typing.Optional[str] = None,
-            aws_session_token: typing.Optional[str] = None,
-            aws_region: typing.Optional[str] = None,
-            timeout: typing.Optional[float] = None,
-    ):
-        AwsClientV2.__init__(
             self,
             service="bedrock",
             aws_access_key=aws_access_key,
