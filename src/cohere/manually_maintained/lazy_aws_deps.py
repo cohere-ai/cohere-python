2b4290a86d1c192cff0c570ab55ace1c698a4799
Billy Trend <billy@cohere.com>
2024-12-20 11:34:28 +0000

    Update error

--- src/cohere/manually_maintained/lazy_aws_deps.py
+++ src/cohere/manually_maintained/lazy_aws_deps.py
@@ -1,23 +1,24 @@
 
+warning = "AWS dependencies are not installed. Please install boto3, botocore, and sagemaker."
 
 def lazy_sagemaker():
     try:
         import sagemaker as sage # type: ignore
         return sage
     except ImportError:
-        raise ImportError("Sagemaker not available. Please install sagemaker.")
+        raise ImportError(warning)
 
 def lazy_boto3():
     try:
         import boto3 # type: ignore
         return boto3
     except ImportError:
-        raise ImportError("Boto3 not available. Please install lazy_boto3().")
+        raise ImportError(warning)
     
 def lazy_botocore():
     try:
         import botocore # type: ignore
         return botocore
     except ImportError:
-        raise ImportError("Botocore not available. Please install botocore.")
+        raise ImportError(warning)
 
