af037f80604848bd52aece7c59a56c3434b7083d
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-02-06 13:54:34 +0000

    SDK regeneration

--- src/cohere/core/client_wrapper.py
+++ src/cohere/core/client_wrapper.py
@@ -24,7 +24,7 @@ class BaseClientWrapper:
         headers: typing.Dict[str, str] = {
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "cohere",
-            "X-Fern-SDK-Version": "5.13.11",
+            "X-Fern-SDK-Version": "5.13.12",
         }
         if self._client_name is not None:
             headers["X-Client-Name"] = self._client_name
