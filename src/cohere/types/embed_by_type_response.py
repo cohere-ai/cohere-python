c4b34695541ed464f6d8dfc64cdbb10e58c8b1d8
Billy Trend <billy@cohere.com>
2025-04-02 15:54:15 +0100

    revert

--- src/cohere/types/embed_by_type_response.py
+++ src/cohere/types/embed_by_type_response.py
@@ -16,7 +16,7 @@ class EmbedByTypeResponse(UncheckedBaseModel):
     An object with different embedding types. The length of each embedding type array will be the same as the length of the original `texts` array.
     """
 
-    texts: typing.Optional[typing.List[str]] = pydantic.Field(default=None)
+    texts: typing.List[str] = pydantic.Field()
     """
     The text entries for which embeddings were returned.
     """
